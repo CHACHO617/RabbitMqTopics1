fc69410a57471e0e4e47c98b7e2da1b2012ba46c
Enrique Merizalde <emerizalde61702@gmail.com>
2023-11-18 13:54:39 -0500

    Initial commit

---
+++ RabbitMq1/Sender.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+class EmailPublisher
+{
+    static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            // Declare the exchange (topic)
+            channel.ExchangeDeclare(exchange: "email_topic", type: ExchangeType.Topic);
+
+            while (true)
+            {
+                Console.Write("Enter recipient email addresses separated by commas (or 'exit' to quit): ");
+                string recipientEmailsInput = Console.ReadLine();
+
+                if (recipientEmailsInput.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                Console.Write("Enter email subject: ");
+                string subject = Console.ReadLine();
+
+                Console.Write("Enter email body: ");
+                string body = Console.ReadLine();
+
+                // Split email addresses and publish a message for each recipient
+                string[] recipientEmails = recipientEmailsInput.Split(',');
+                foreach (var recipientEmail in recipientEmails)
+                {
+                    // Format the email message
+                    string emailMessage = $"{subject}\n\n{body}";
+
+                    // Publish the email to the topic with routing key as the email address
+                    var bodyBytes = Encoding.UTF8.GetBytes(emailMessage);
+                    channel.BasicPublish(exchange: "email_topic", routingKey: recipientEmail.Trim(), basicProperties: null, body: bodyBytes);
+
+                    Console.WriteLine($"Email sent to {recipientEmail.Trim()}");
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMq2/Reciever.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+class EmailConsumer
+{
+    static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            // Declare the exchange (topic)
+            channel.ExchangeDeclare(exchange: "email_topic", type: ExchangeType.Topic);
+
+            // Declare a common queue
+            var queueName = "common_email_queue";
+            channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false);
+
+            // Bind the common queue to the exchange with a routing key (use "#" to receive all messages)
+            channel.QueueBind(queue: queueName, exchange: "email_topic", routingKey: "#");
+
+            Console.WriteLine("Waiting for emails. To exit press Ctrl+C");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var emailMessage = Encoding.UTF8.GetString(body);
+                var recipientEmail = ea.RoutingKey;
+
+                // Process the email message (e.g., send email)
+                SendEmail(recipientEmail, emailMessage);
+
+                Console.WriteLine($"Email sent to {recipientEmail}");
+            };
+
+            // Start consuming messages from the queue
+            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+            Console.ReadLine(); // Keep the application running
+        }
+    }
+
+    static void SendEmail(string recipientEmail, string emailMessage)
+    {
+        // Configure your SMTP settings
+        var smtpClient = new SmtpClient("smtp.gmail.com")
+        {
+            Port = 587,
+            Credentials = new NetworkCredential("emerizalde.alt@gmail.com", "nocr cixc kvxi gyal"),
+            EnableSsl = true,
+        };
+
+        // Create and send the email
+        var mail = new MailMessage("emerizalde.alt@gmail.com", recipientEmail)
+        {
+            Subject = "Email from RabbitMQ",
+            Body = emailMessage,
+            IsBodyHtml = false
+        };
+
+        smtpClient.Send(mail);
+    }
+}
+
+
+
+
